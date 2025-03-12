a4eec10e4f85a7095f7a3fce30cd5238ccf254f9
Alvaro <alvarolukmanto@gmail.com>
2025-03-12 23:54:58 +0800

    add useful tensor operations examples

--- tensor-examples/02-math-and-comparison.py
+++ tensor-examples/02-math-and-comparison.py
@@ -60,4 +60,31 @@ p = 4
 mtx1 = torch.rand((32, m, n))
 mtx2 = torch.rand((32, n, p))
 bmm = mtx1.bmm(mtx2)
-print(bmm)
+
+# Concept: Broadcasting
+# Automatically match the matrix size so that element wise operation can be done
+# Example:
+
+tsr1 = torch.rand((5, 5))
+tsr2 = torch.rand((1, 5)) # 4 identical rows will be created to do operations with tsr1
+
+z = tsr1 - tsr2 # pytorch will apply broadcasting
+z = tsr1 ** tsr2 # perform element wise exponentiation
+
+# Other useful tensor operations
+# Read about tensor dimensions here 
+# https://medium.com/analytics-vidhya/an-intuitive-understanding-on-tensor-sum-dimension-with-pytorch-d9b0b6ebbae
+sum_x = torch.sum(x, dim = 0)
+values, indices = torch.max(x, dim = 0)
+values, indices = torch.min(x, dim = 0)
+abs_x = torch.abs(x)
+argmax_x = torch.argmax(x, dim = 0) # indices of max
+argmin_x = torch.argmin(x, dim = 0)
+mean_x = torch.mean(x.float(), dim = 0)
+is_equal = torch.eq(x, y)
+
+sorted = torch.sort(torch.rand((2, 3, 3)), dim = 0, descending=False)
+clamped = torch.clamp(x, min = 2, max = 5)
+
+contains_true = torch.any(torch.tensor([1, 0, 1, 1, 1], dtype=torch.bool))
+all_truthy = torch.all(torch.tensor([1, 0, 1, 1, 1], dtype=torch.bool))
